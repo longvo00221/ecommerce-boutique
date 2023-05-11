1de1de658e24387bc3fc1284cc3c22a8c6de4906
longvo00221 <longvo010203@gmail.com>
2023-05-11 22:00:04 +0700

    .

--- server/index.js
+++ server/index.js
@@ -19,13 +19,13 @@ app.use(cookieParser());
 const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 mongoose.connect(process.env.MONGODB_URL)
-.then(() => {
-  console.log("DB Connected");
-  server.listen(port, () => {
-    console.log(`Server is Running on Port ${port}`);
-  });
-})
-.catch((err)=>{
-    console.log({err});
-    process.exit(1);
-})
+    .then(() => {
+        console.log("DB Connected");
+        server.listen(port, () => {
+            console.log(`Server is Running on Port ${port}`);
+        });
+    })
+    .catch((err)=>{
+        console.log({err});
+        process.exit(1);
+    })
---
+++ server/src/middlewares/token.middleware.js
@@ -0,0 +1,2 @@
+import jsonwebtoken from "jsonwebtoken";
+import responseHandler from "../handlers/reponse.handler.js";
