9040206e9448a3d2c1b0d703dfef766896d99927
longvo00221 <longvo010203@gmail.com>
2023-05-12 16:09:20 +0700

    .

---
+++ client/src/components/common/Header/HeaderNav.jsx
@@ -0,0 +1,10 @@
+
+const HeaderNav = () => {
+  return (
+    <div>
+      <div className="max-w-[1170px] mx-auto">Header</div>
+    </div>
+  )
+}
+
+export default HeaderNav
---
+++ client/src/components/layout/MainLayout.jsx
@@ -0,0 +1,14 @@
+import HeaderNav from "../common/Header/HeaderNav"
+
+const MainLayout = () => {
+  return (
+    <div>
+        {/* header */}
+        <HeaderNav/>
+        {/* body */}
+        <>body</>
+    </div>
+  )
+}
+
+export default MainLayout
